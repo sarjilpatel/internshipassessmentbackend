82ca2ff471187fd3fe50f9c9838980281847b784
sarjil patel <sarjilpatel2903@gmail.com>
2023-02-05 17:57:35 +0530

    changes in cors

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+var cors = require("cors");
 
 require("dotenv").config({ path: "configs/config.env" });
 
 //Using middlewares
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(
+  cors({
+    origin: "https://sarjil-internshipassessment.netlify.app/",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 
 //Importing Routers
 const userRoutes = require("./routes/userRoutes");
